2bc71245ef264ed0f36158ad11b3985ea6f28af9
mickeyminion14 <sarthakagrawal6@gmail.com>
2025-10-12 22:47:31 +0530

    feat: Setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,15 @@
+import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World App</h1>
+        <h1 className="text-2xl font-bold">Hello World Apps/web</h1>
         <Button size="sm">Button</Button>
+        <p>{add(2, 4)}</p>
+        <Input />
       </div>
     </div>
   );
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,3 +1,4 @@
+import { subtract } from "@workspace/math/subtract";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
@@ -6,6 +7,7 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World Widget</h1>
         <Button size="sm">Button</Button>
+        <p>{subtract(2, 2)}</p>
       </div>
     </div>
   );
